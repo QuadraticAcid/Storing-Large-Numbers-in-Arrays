426c550ffaa96bbfb5d2dfb56e3af6e5f453fbbf
QuadraticAcid <51001041+QuadraticAcid@users.noreply.github.com>
2019-06-18 09:41:46 -0700

    Create division.h

---
+++ division.h
@@ -0,0 +1,8 @@
+#ifndef DIVISION_H
+#define DIVISION_H
+
+#include "structure.h"
+
+extern void divide(struct int_struct numerator, struct int_struct denominator, struct int_struct *dest);
+
+#endif
