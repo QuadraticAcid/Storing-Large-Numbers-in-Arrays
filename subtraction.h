e861e4db74e1e71f92f2bbc4e467515cd8d99c9d
QuadraticAcid <51001041+QuadraticAcid@users.noreply.github.com>
2019-06-18 09:59:25 -0700

    Update subtraction.h

--- subtraction.h
+++ subtraction.h
@@ -6,4 +6,5 @@
 //Functions
 extern void subtract(struct int_struct minuend, struct int_struct subtrahend, struct int_struct *dest);
 extern int greaterthan(struct int_struct a, struct int_struct b);
+
 #endif
